21ec97f3934949371984f0bf658bb785c361f43b
Sean Joseph <theseanjoseph@gmail.com>
2020-08-06 10:58:19 -0700

    Added ohmyzsh installation

--- src/system_setup/zsh.py
+++ src/system_setup/zsh.py
@@ -11,18 +11,22 @@ def install():
     filesystem.create_directory_if_not_exists("~/.local/etc/dot-files")
 
     git.clone_repository_if_not_exists(
-        "https://github.com/joseph62/dot-files", "~/temp/dot-files"
+        "https://github.com/joseph62/dot-files.git", "~/temp/dot-files"
     )
     git.clone_repository_if_not_exists(
-        "https://github.com/zsh-users/zsh-syntax-highlighting",
+        "https://github.com/zsh-users/zsh-syntax-highlighting.git",
         "~/.local/opt/zsh-syntax-highlighting",
     )
+    git.clone_repository_if_not_exists(
+        "https://github.com/ohmyzsh/ohmyzsh.git",
+        "~/.local/opt/ohmyzsh",
+    ) 
 
     filesystem.update_file(
-        "~/temp/dot-files/aliases", "~/.local/etc/dot-files/.aliases"
+        "~/temp/dot-files/aliases", "~/.local/etc/dot-files/aliases"
     )
     filesystem.update_file(
-        "~/temp/dot-files/environment", "~/.local/etc/dot-files/.environment"
+        "~/temp/dot-files/environment", "~/.local/etc/dot-files/environment"
     )
     filesystem.update_file("~/temp/dot-files/zshrc", "~/.zshrc")
 
