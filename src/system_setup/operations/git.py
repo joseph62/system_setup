e821a0c2bbdd67beaab26ef851ac90e2bdc6f915
Sean Joseph <theseanjoseph@gmail.com>
2020-08-21 19:17:04 -0400

    Added logic to update existing git repos

--- src/system_setup/operations/git.py
+++ src/system_setup/operations/git.py
@@ -4,15 +4,26 @@ import logging
 from . import process
 
 
+def _clone(repository, path):
+    return subprocess.run(["git", "clone", repository, path])
+
+
+def _update(path):
+    return subprocess.run(["git", "pull", "origin"], cwd=path)
+
+
 def clone_repository_if_not_exists(repository, path):
     expanded_path = os.path.expanduser(path)
     if os.path.exists(expanded_path):
-        logging.warn(f"{expanded_path} already exists, not cloning {repository}!")
+        logging.warn(f"{expanded_path} already exists, updating repository.")
+        git_return = _update(expanded_path)
+        if process.successful_process(git_return):
+            logging.info(f"{expanded_path} successfully updated")
+        else:
+            logging.error(f"{expanded_path} failed to update")
     else:
-        git = subprocess.run(["git", "clone", repository, expanded_path])
-        if process.successful_process(git):
+        git_return = _clone(repository, expanded_path)
+        if process.successful_process(git_return):
             logging.info(f"{repository} successfully cloned to {expanded_path}.")
         else:
-            logging.error(
-                f"{repository} was not successfully cloned to {expanded_path}!"
-            )
+            logging.error(f"{repository} failed clone to {expanded_path}!")
