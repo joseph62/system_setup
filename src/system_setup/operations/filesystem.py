7dd4874814af46b1c4a7f0760f822807f1e73148
Sean Joseph <theseanjoseph@gmail.com>
2020-08-21 18:49:59 -0400

    Added logic to not attempt removal of a path if it has already happened

--- src/system_setup/operations/filesystem.py
+++ src/system_setup/operations/filesystem.py
@@ -5,6 +5,8 @@ import collections
 
 FilePath = collections.namedtuple("FilePath", ["path", "name"])
 
+UPDATED_PATHS = set()
+REMOVED_PATHS = set()
 
 def get_all_files(path):
     expanded_path = os.path.expanduser(path)
@@ -40,19 +42,26 @@ def backup_file(path):
 def update_file(source_path, destination_path):
     expanded_source_path = os.path.expanduser(source_path)
     expanded_destination_path = os.path.expanduser(destination_path)
-    if os.path.exists(expanded_destination_path):
-        logging.warn(f"{expanded_destination_path} already exists")
-        backup_file(expanded_destination_path)
-    shutil.copy2(expanded_source_path, expanded_destination_path)
-    logging.info(f"{expanded_source_path} copied to {expanded_destination_path}")
+    if expanded_destination_path in UPDATED_PATHS:
+        logging.info(f"{expanded_destination_path} has already been updated this run.")
+    else:
+        if  UPDATED_PATHS and os.path.exists(expanded_destination_path):
+            logging.warn(f"{expanded_destination_path} already exists")
+            backup_file(expanded_destination_path)
+        shutil.copy2(expanded_source_path, expanded_destination_path)
+        UPDATED_PATHS.add(expanded_destination_path)
+        logging.info(f"{expanded_source_path} copied to {expanded_destination_path}")
 
 
 def remove_directory_if_exists(path):
     expanded_path = os.path.expanduser(path)
-    if os.path.isdir(expanded_path):
+    if expanded_path in REMOVED_PATHS:
+        logging.info(f"{expanded_path} has already been removed")
+    elif os.path.isdir(expanded_path):
         logging.info(f"Removing {expanded_path}")
         shutil.rmtree(expanded_path, ignore_errors=True)
     elif os.path.exists(expanded_path):
         logging.error(f"Unable to remove {expanded_path} because it is not a directory")
     else:
         logging.warn(f"Unable to remove {expanded_path} because it does not exist")
+    REMOVED_PATHS.add(expanded_path)
