6091d7075890e6e15e03a191c54d7410d7220b72
Sean Joseph <theseanjoseph@gmail.com>
2020-09-04 11:04:36 -0400

    Added helper functions to package_manager to make interface more bearable

--- src/system_setup/operations/package_manager.py
+++ src/system_setup/operations/package_manager.py
@@ -44,3 +44,23 @@ def install_program_if_not_exists(program, package_manager_options):
                 else:
                     logging.warn(f"Failed to install {program}")
                 break
+
+
+def brew(packages):
+    return ("brew", f"brew install {' '.join(packages)}")
+
+
+def apt(packages):
+    return ("apt", f"sudo apt install {' '.join(packages)}")
+
+
+def dnf(packages):
+    return ("dnf", f"sudo dnf install {' '.join(packages)}")
+
+
+def options(*package_managers):
+    return dict(package_managers)
+
+
+def same_package(package, *package_managers):
+    return [package_manager(package) for package_manager in package_managers]
