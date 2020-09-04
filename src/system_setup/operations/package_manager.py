ffb32444c077be4ef1c7f6d5f04f17c67a72a5ee
Sean Joseph <theseanjoseph@gmail.com>
2020-09-04 11:23:09 -0400

    Modified package manager helpers to use varargs

--- src/system_setup/operations/package_manager.py
+++ src/system_setup/operations/package_manager.py
@@ -46,15 +46,15 @@ def install_program_if_not_exists(program, package_manager_options):
                 break
 
 
-def brew(packages):
+def brew(*packages):
     return ("brew", f"brew install {' '.join(packages)}")
 
 
-def apt(packages):
+def apt(*packages):
     return ("apt", f"sudo apt install {' '.join(packages)}")
 
 
-def dnf(packages):
+def dnf(*packages):
     return ("dnf", f"sudo dnf install {' '.join(packages)}")
 
 
