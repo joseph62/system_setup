2dd54244a95026625c5a4cf382a603065d4cd1e1
Sean Joseph <theseanjoseph@gmail.com>
2020-09-04 11:04:46 -0400

    Added packages installer

---
+++ src/system_setup/packages.py
@@ -0,0 +1,15 @@
+from .operations import package_manager
+from .operations.package_manager import options, brew, apt, dnf, same_package
+
+
+def install():
+    package_manager.install_program_if_not_exists(
+        "rlwrap", options(*same_package("rlwrap", brew, apt, dnf))
+    )
+    package_manager.install_program_if_not_exists(
+        "http", options(*same_package("httpie", brew, apt, dnf))
+    )
+
+
+def cleanup():
+    pass
